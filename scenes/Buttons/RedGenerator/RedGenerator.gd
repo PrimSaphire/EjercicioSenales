df511cd3f49c2d8cb714cdc10808d5dcc7969117
PrimSaphire <luz_oscurallive.com.ar>
2020-04-14 13:51:23 -0300

    Agregada la func generate blocks

--- scenes/Buttons/RedGenerator/RedGenerator.gd
+++ scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -1,6 +1,12 @@
 extends TextureButton
 
+var BlockRed = preload("res://scenes/Blocks/Red/BlockRed.tscn")
 
+func generate_block():
+	var BlockRedInstance = BlockRed.instance()
+	add_child(BlockRedInstance)
+	pass
 
 func _on_RedGenerator_button_down():
+	generate_block()
 	pass
--- scenes/Buttons/VioletGenerator/VioletGenerator.gd
+++ scenes/Buttons/VioletGenerator/VioletGenerator.gd
@@ -1,6 +1,12 @@
 extends TextureButton
 
+var BlockViolet = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
 
+func generate_block():
+	var BlockVioletInstance = BlockViolet.instance()
+	add_child(BlockVioletInstance)
+	pass
 
 func _on_VioletGenerator_button_down():
+	generate_block()
 	pass 
